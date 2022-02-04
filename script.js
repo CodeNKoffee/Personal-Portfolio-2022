68dc79424dded3eee3ccc7c737c28bbaf5af9018
Hatem Soliman <hatemthedev@gmail.com>
2022-02-04 02:18:39 +0200

    Added footer logo link redirection

---
+++ script.js
@@ -0,0 +1,40 @@
+//VARIABLES
+
+
+//EVENT LISTENERS
+
+
+//FUNCTIONS
+//Navigation Bar & Hamburger
+function openMenu(){
+  document.body.classList += " menu--open"
+}
+
+function closeMenu(){
+  document.body.classList.remove('menu--open')
+}
+
+//Change Important Links Colour depending on Background Colour
+const link = document.querySelector('.imp__link--anchor');
+const sections = document.querySelector('.white-sections');
+
+const linkObserverCallback = (watchEntry, linkObserver) => {
+  if(!watchEntry[0].isIntersecting) {
+    link.classList.remove('dark-theme');
+    link.classList.add('imp__link--anchor')
+  } else {
+    link.classList.remove('imp__link--anchor');
+    link.classList.add('dark-theme');
+  }
+}
+
+const linkObserverOptions = {
+  rootMargin: '-110px',
+  threshold: 0
+}
+
+const linkObserver = new IntersectionObserver(linkObserverCallback, linkObserverOptions);
+
+linkObserver.observe(sections);
+
+
