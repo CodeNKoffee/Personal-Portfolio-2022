8107281b06a9cccad088e513bbe71fc970900526
Hatem <hatemthedev@gmail.com>
2022-02-09 22:13:35 +0200

    All social links change colour when entering secondt white section

--- script.js
+++ script.js
@@ -8,7 +8,15 @@ function closeMenu(){
   document.body.classList.remove('menu--open')
 }
 
+
+
+
+
+
+
+
 //Change Important Links Colour depending on Background Colour
+//FOR FIRST WHITE SECTION
 //First Link
 const link = document.querySelector('.div1');
 const sections = document.querySelector('.white-sections');
@@ -111,5 +119,111 @@ const fourthLinkObserverOptions = {
 const fourthLinkObserver = new IntersectionObserver(fourthLinkObserverCallback, fourthLinkObserverOptions);
 
 fourthLinkObserver.observe(fourthSections);
+
+
+
+
+//FOR SECOND WHITE SECTION
+const linkFirst = document.querySelector('.div1');
+const sectionsFirst = document.querySelector('.white-section-2');
+
+
+const linkObserverCallbackFirst = (watchEntry, linkObserverFirst) => {
+  if(!watchEntry[0].isIntersecting) {
+    linkFirst.classList.remove('dark-theme');
+    linkFirst.classList.add('imp__link--anchor')
+  } else {
+    linkFirst.classList.remove('imp__link--anchor');
+    linkFirst.classList.add('dark-theme');
+  }
+}
+
+const linkObserverOptionsFirst = {
+  rootMargin: '-110px',
+  threshold: 0
+}
+
+
+const linkObserverFirst = new IntersectionObserver(linkObserverCallbackFirst, linkObserverOptionsFirst);
+
+linkObserverFirst.observe(sectionsFirst);
+
+
+//Second Link
+const linkSec = document.querySelector('.div2');
+const sectionsSec = document.querySelector('.white-section-2');
+
+
+const linkObserverCallbackSec = (watchEntry, linkObserverSec) => {
+  if(!watchEntry[0].isIntersecting) {
+    linkSec.classList.remove('dark-theme');
+    linkSec.classList.add('imp__link--anchor')
+  } else {
+    linkSec.classList.remove('imp__link--anchor');
+    linkSec.classList.add('dark-theme');
+  }
+}
+
+const linkObserverOptionsSec = {
+  rootMargin: '-110px',
+  threshold: 0
+}
+
+
+const linkObserverSec = new IntersectionObserver(linkObserverCallbackSec, linkObserverOptionsSec);
+
+linkObserverSec.observe(sectionsSec);
+
+
+//Third Link
+const linkThird = document.querySelector('.div3');
+const sectionsThird = document.querySelector('.white-section-2');
+
+
+const linkObserverCallbackThird = (watchEntry, linkObserverThird) => {
+  if(!watchEntry[0].isIntersecting) {
+    linkThird.classList.remove('dark-theme');
+    linkThird.classList.add('imp__link--anchor')
+  } else {
+    linkThird.classList.remove('imp__link--anchor');
+    linkThird.classList.add('dark-theme');
+  }
+}
+
+const linkObserverOptionsThird = {
+  rootMargin: '-110px',
+  threshold: 0
+}
+
+
+const linkObserverThird = new IntersectionObserver(linkObserverCallbackThird, linkObserverOptionsThird);
+
+linkObserverThird.observe(sectionsThird);
+
+
+//Third Link
+const linkFourth = document.querySelector('.div4');
+const sectionsFourth = document.querySelector('.white-section-2');
+
+
+const linkObserverCallbackFourth = (watchEntry, linkObserverFourth) => {
+  if(!watchEntry[0].isIntersecting) {
+    linkFourth.classList.remove('dark-theme');
+    linkFourth.classList.add('imp__link--anchor')
+  } else {
+    linkFourth.classList.remove('imp__link--anchor');
+    linkFourth.classList.add('dark-theme');
+  }
+}
+
+const linkObserverOptionsFourth = {
+  rootMargin: '-110px',
+  threshold: 0
+}
+
+
+const linkObserverFourth = new IntersectionObserver(linkObserverCallbackFourth, linkObserverOptionsFourth);
+
+linkObserverFourth.observe(sectionsFourth);
 
 
