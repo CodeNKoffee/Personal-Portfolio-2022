5ea83567d5d4744f6072fdfe801440a0836c5117
Hatem Soliman <hatemthedev@gmail.com>
2022-02-26 16:44:19 +0200

    Git issues solved

--- script.js
+++ script.js
@@ -360,24 +360,3 @@ const circleLinkObserverFour = new IntersectionObserver(circleLinkObserverCallba
 circleLinkObserverFour.observe(contactSection);
 
 
-
-
-<<<<<<< HEAD
-=======
-
-
-
-
-//Recapthcha
-document.getElementById("contact__submit").addEventListener("click", () => {
-  if (document.getElementById("recapthcha").addEventListener("!click")) {
-    const para = document.createElement("p");
-    const node = document.createTextNode("Please verify you're not a robot");
-    document.getElementById("contact__form").appendChild("para")
-    para.appendChild("node");
-    node.classList.add("warning")
-  } else {
-    continue;
-  }
-})
->>>>>>> 29670fb98042d8b79931f92add986c38e72e0481
